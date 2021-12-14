edd833d05197b31f1e074385124960d1913078c8
Avichay <avichaymeiri@gmail.com>
2021-12-14 21:50:54 +0200

    add style in sign_in activity

--- app/src/main/java/com/meiri/caloriecalculator/SignInActivity.kt
+++ app/src/main/java/com/meiri/caloriecalculator/SignInActivity.kt
@@ -27,10 +27,10 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
         Log.d(TAG, "[onCreate] initialize activity")
         user = User(intent.getStringExtra("user_id")!!)
 
-        birthDateEditText = findViewById(R.id.birth_date_edit_text)
-        registerButton = findViewById(R.id.register_button)
-        genderLayout = findViewById(R.id.gender_layout)
-        activityFactorLayout = findViewById(R.id.activity_factor_layout)
+//        birthDateEditText = findViewById(R.id.birth_date_edit_text)
+//        registerButton = findViewById(R.id.register_button)
+//        genderLayout = findViewById(R.id.gender_layout)
+//        activityFactorLayout = findViewById(R.id.activity_factor_layout)
 
         birthDateEditText.addTextChangedListener(DateTextWatcher())
         registerButton.setOnClickListener(this)
@@ -39,10 +39,10 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
     private fun returnUserInfo() {
         val userData: HashMap<String, Any> = HashMap()
         userData["birth_date"] = birthDateEditText.text.toString()
-        userData["height"] = findViewById<EditText>(R.id.height_edit_text).text.toString().toLong()
-        userData["weight"] = findViewById<EditText>(R.id.weight_edit_text).text.toString().toLong()
-        userData["gender"] = findViewById<RadioButton>(genderLayout.checkedRadioButtonId).text.toString()
-        userData["activity_factor"] = findViewById<RadioButton>(activityFactorLayout.checkedRadioButtonId).text.toString().toDouble()
+//        userData["height"] = findViewById<EditText>(R.id.height_edit_text).text.toString().toLong()
+//        userData["weight"] = findViewById<EditText>(R.id.weight_edit_text).text.toString().toLong()
+//        userData["gender"] = findViewById<RadioButton>(genderLayout.checkedRadioButtonId).text.toString()
+//        userData["activity_factor"] = findViewById<RadioButton>(activityFactorLayout.checkedRadioButtonId).hint.toString().toDouble()
 
         val resultIntent = Intent()
         resultIntent.putExtra("user_data", userData)
